4961a5a11da1255ad77ed5fe2e2be890c2b36a79
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2023-10-30 15:21:45 +0300

    database error

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost:27017/')
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/Users'),
   ],
   controllers: [AppController],
   providers: [AppService],
